dc7bf5a7099094083cd11386c7d2a25a2da1f38e
grinat <paladin2012gnu@gmail.com>
2019-05-20 00:30:26 +0300

    feat: added static server script for herkou

---
+++ static.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(__dirname + '/dist'))
+
+// history fallback
+app.use(function(req, res){
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
+const port = process.env.PORT || 5166
+
+app.listen(port, () => {
+  console.log(`Static server run at http://localhost:${port}`)
+})
